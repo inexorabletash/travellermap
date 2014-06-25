237e74ae8378c4f8d2bc7c5b334f2ec88c04df0b
Joshua Bell <inexorabletash@gmail.com>
2014-06-25 13:32:42 -0700

    WIP - content tests

---
+++ test/ContentTest.js
@@ -0,0 +1,89 @@
+(function(global) {
+
+  function diff(text1, text2) {
+    var lines1 = text1.split(/\r?\n/);
+    var lines2 = text2.split(/\r?\n/);
+
+    // TODO: Real diff - for now this just ignores leading/trailing matches
+    while (lines1.length && lines2.length) {
+      if (lines1[0] === lines2[0]) {
+        lines1.shift();
+        lines2.shift();
+        continue;
+      }
+      if (lines1[lines1.length - 1] === lines2[lines2.length - 1]) {
+        lines1.pop();
+        lines2.pop();
+        continue;
+      }
+      break;
+    }
+
+    var out = [];
+    while (lines1.length && lines2.length) {
+      out.push('- ' + lines1.shift());
+      out.push('+ ' + lines2.shift());
+    }
+    while (lines1.length)
+      out.push('- ' + lines1.shift());
+    while (lines2.length)
+      out.push('+ ' + lines2.shift());
+
+    return out.join('\n');
+  }
+
+  // TODO: Use fetch/Response from http://fetch.spec.whatwg.org
+  function fetch(url, options) {
+    return new Promise(function(resolve, reject) {
+      var xhr = new XMLHttpRequest(), async = true;
+      xhr.open(options && options.method || 'GET', url, async);
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState !== XMLHttpRequest.DONE)
+          return;
+        if (xhr.status === 200)
+          resolve(xhr);
+        else
+          reject(xhr.responseText);
+      };
+      xhr.onerror = reject;
+      xhr.send();
+    });
+  }
+
+  function compareContent(contentType, url1, url2, callback) {
+    Promise.all([fetch(url1), fetch(url2)]).then(function(responses) {
+      var a = 'Content-Type: ' + contentType + '\n'
+            + responses[0].responseText;
+      var b = 'Content-Type: ' + responses[1].getResponseHeader('Content-Type') + '\n'
+            + responses[1].responseText;
+      var d = diff(a, b);
+      callback(a, b, d, !d);
+    }).catch(function(error) {
+      callback('', '', 'Fetch failed: ' + error, true);
+      // TODO: Handle error
+    });
+  }
+
+  var $ = function(selector) { return document.querySelector(selector); };
+  var ce = function(tag) { return document.createElement(tag); };
+  var tn = function(text) { return document.createTextNode(text); };
+
+  function check(contentType, url1, url2) {
+    var tr = $('#results').appendChild(ce('tr'));
+    tr.appendChild(ce('td')).appendChild(tn(url1));
+    tr.appendChild(ce('td')).appendChild(tn(url2));
+    tr.appendChild(ce('td')).appendChild(tn('diff'));
+
+    tr = $('#results').appendChild(ce('tr'));
+
+    compareContent(contentType, '../' + url1, '../' + url2, function(a, b, c, pass) {
+      for (var i = 0; i < 3; ++i) {
+        tr.appendChild(ce('td')).appendChild(tn(arguments[i]));
+      }
+      tr.className = pass ? 'pass' : 'fail';
+    });
+  }
+
+  global.check = check;
+
+}(self));
