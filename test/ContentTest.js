c6bad3fd389f7af55d2bc21f1bcf5409541c3e61
Joshua Bell <inexorabletash@gmail.com>
2014-11-16 19:25:20 -0800

    Content tests: start fleshing them out

--- test/ContentTest.js
+++ test/ContentTest.js
@@ -32,43 +32,11 @@
     return out.join('\n');
   }
 
-  // TODO: Use a proper polyfill for fetch/Response from http://fetch.spec.whatwg.org
-  function Response(xhr) {
-    this._xhr = xhr;
-    var headers = {};
-    xhr.getAllResponseHeaders().split(/\r?\n/).forEach(function(header) {
-      headers[header.substring(0, header.indexOf(':'))] = header.substring(header.indexOf(':') + 2);
-    });
-    this.headers = headers;
-  }
-  Response.prototype = {
-    asText: function() {
-      return Promise.resolve(this._xhr.responseText);
-    },
-    asXML: function() {
-      return Promise.resolve(this._xhr.responseXML);
-    }
-  };
-  function fetch(url, options) {
-    return new Promise(function(resolve, reject) {
-      var xhr = new XMLHttpRequest(), async = true;
-      xhr.open(options && options.method || 'GET', url, async);
-      xhr.onreadystatechange = function() {
-        if (xhr.readyState !== XMLHttpRequest.DONE)
-          return;
-        if (xhr.status === 200)
-          resolve(new Response(xhr));
-        else
-          reject(xhr.statusText);
-      };
-      xhr.send();
-    });
-  }
 
   function compareContent(contentType, url1, url2, callback) {
 
     function getHeaderAndBody(response) {
-      return response.asText().then(function(text) {
+      return response.text().then(function(text) {
         return {
           headers: response.headers,
           text: text
@@ -76,11 +44,12 @@
       });
     }
 
-    Promise.all([fetch(url1).then(getHeaderAndBody), fetch(url2).then(getHeaderAndBody)])
+    Promise.all([fetch(url1).then(getHeaderAndBody),
+                 fetch(url2, {headers: {Accept: contentType }}).then(getHeaderAndBody)])
       .then(function(responses) {
         var a = 'Content-Type: ' + contentType + '\n'
               + responses[0].text;
-        var b = 'Content-Type: ' + responses[1].headers['Content-Type'] + '\n'
+        var b = 'Content-Type: ' + responses[1].headers.get('Content-Type') + '\n'
               + responses[1].text;
         var d = diff(a, b);
         callback(a, b, d, !d);
@@ -102,10 +71,10 @@
 
     tr = $('#results').appendChild(elem('tr'));
 
-    compareContent(contentType, '../' + url1, '../' + url2, function(a, b, c, pass) {
-      tr.appendChild(elem('td')).appendChild(text(a));
-      tr.appendChild(elem('td')).appendChild(text(b));
-      tr.appendChild(elem('td')).appendChild(text(c));
+    compareContent(contentType, url1, url2, function(a, b, c, pass) {
+      tr.appendChild(elem('td')).appendChild(elem('textarea')).value = a;
+      tr.appendChild(elem('td')).appendChild(elem('textarea')).value = b;
+      tr.appendChild(elem('td')).appendChild(elem('textarea')).value = c;
       tr.classList.add(pass ? 'pass' : 'fail');
     });
   }
