d247d7934e41e201acc0474984059234c952ee5c
Joshua Bell <inexorabletash@gmail.com>
2015-05-15 13:20:32 -0700

    Generate a TOC for credits

---
+++ doc/toc.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', function() {
+  var $ = function(s) { return document.querySelector(s); };
+  var $$ = function(s) { return document.querySelectorAll(s); };
+  console.log('hi');
+
+  var toc = document.createElement('nav');
+  toc.className = 'toc';
+  [].slice.call($$('h2,h3,h4,h5,h6')).forEach(function(h) {
+    var a = document.createElement('a');
+    var text = h.textContent || h.innerText;
+    if (!h.id)
+      h.id = text.toLowerCase().replace(/\s+/g, '-').replace(/[^a-z-]/g, '');;
+    a.href = '#' + h.id;
+    a.className = h.tagName;
+    a.appendChild(document.createTextNode(text));
+    toc.appendChild(a);
+  });
+  var title = document.createElement('h2');
+  title.appendChild(document.createTextNode('Contents'));
+  toc.insertBefore(title, toc.firstChild);
+  document.body.appendChild(toc);
+});
