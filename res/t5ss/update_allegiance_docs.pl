a28fa0a841da9d27c0f824a91204d9fd0869f728
Joshua Bell <inexorabletash@gmail.com>
2015-06-18 09:38:51 -0700

    Script to update allegiance docs

---
+++ res/t5ss/update_allegiance_docs.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/env perl
+use strict;
+use File::Basename;
+
+my $dir = dirname($0);
+
+my $html_path = $dir . '/../../doc/secondsurvey.html';
+my $html;
+{
+    open my $fh, '<', $html_path or die;
+    local $/ = undef;
+    $html = <$fh>;
+    close $fh;
+}
+
+
+my $alleg_path = $dir . '/allegiance_codes.tsv';
+my @lines;
+{
+    open my $fh, '<', $alleg_path or die;
+    while (<$fh>) {
+        next unless m/^(\w\w\w\w)\t(.*)/;
+        next if $1 eq "Code";
+        push @lines, "      <tr><td><code>$1</code><td>$2";
+    }
+    close $fh;
+}
+
+@lines = sort @lines;
+
+my $replace = join("\n", @lines);
+
+$html =~ s/(<!-- Allegiance Table Begin -->\s*\n)(.*?)(\n\s*<!-- Allegiance Table End -->)/$1$replace$3/s;
+
+{
+    open my $fh, '>', $html_path or die;
+    print $fh $html;
+    close $fh;
+}
