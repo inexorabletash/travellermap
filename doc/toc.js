6288d913a59aebe5229db2d1276826ef25761e03
Joshua Bell <inexorabletash@gmail.com>
2015-09-15 19:25:29 -0700

    Allow digits in auto-generated ids

--- doc/toc.js
+++ doc/toc.js
@@ -8,7 +8,7 @@ window.addEventListener('DOMContentLoaded', function() {
     var a = document.createElement('a');
     var text = h.textContent || h.innerText;
     if (!h.id)
-      h.id = text.toLowerCase().replace(/\s+/g, '-').replace(/[^a-z-]/g, '');;
+      h.id = text.toLowerCase().replace(/\s+/g, '-').replace(/[^a-z0-9-]/g, '');;
     a.href = '#' + h.id;
     a.className = h.tagName;
     a.appendChild(document.createTextNode(text));
