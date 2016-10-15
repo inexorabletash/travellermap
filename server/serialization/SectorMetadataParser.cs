45f0970606fdf52ff533f66ff04f3aebc257317d
Joshua Bell <inexorabletash@gmail.com>
2016-10-15 12:25:11 -0700

    Show more detailed XML parsing errors

--- server/serialization/SectorMetadataParser.cs
+++ server/serialization/SectorMetadataParser.cs
@@ -64,8 +64,17 @@ namespace Maps.Serialization
 
         public override Sector Parse(TextReader reader)
         {
-            XmlSerializer xs = new XmlSerializer(typeof(Sector));
-            return xs.Deserialize(reader) as Sector;
+            try
+            {
+                XmlSerializer xs = new XmlSerializer(typeof(Sector));
+                return xs.Deserialize(reader) as Sector;
+            }
+            catch (System.InvalidOperationException ex)
+            {
+                if (ex.InnerException is System.Xml.XmlException)
+                    throw ex.InnerException;
+                throw;
+            }
         }
     }
 }
