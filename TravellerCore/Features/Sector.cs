a62296d4a06ac68d5efc349f4e30a48e52212863
Julian Silden Langlo <langlo94@gmail.com>
2021-11-02 20:03:55 +0100

    Update parser.

--- TravellerCore/Features/Sector.cs
+++ TravellerCore/Features/Sector.cs
@@ -5,4 +5,13 @@ public class Sector : IWorldHolder
     public List<World> Worlds {  get; init; }
     public Position Position {  get; init; }
     public List<Quadrant> Quadrants { get; init; }
+    public List<SubSector> SubSectors
+    {
+        get
+        {
+            var subSectors = new List<SubSector>();
+            foreach (var quadrant in Quadrants) subSectors.AddRange(quadrant.SubSectors);
+            return subSectors;
+        }
+    }
 }
--- TravellerParser/JsonParser.cs
+++ TravellerParser/JsonParser.cs
@@ -9,6 +9,18 @@ public class JsonParser: IParser
     public bool TryParseSector(string inputSector, string? inputMetadata, out Sector result)
     {
         result = JsonSerializer.Deserialize<Sector>(inputSector);
+
+        foreach  (var world in result.Worlds)
+        {
+            foreach (var quadrant in result.Quadrants)
+                if (world.Position.IsInQuadrant(quadrant.Position))
+                    quadrant.Worlds.Add(world);
+
+            foreach (var subSector in result.Quadrants)
+                if (world.Position.IsInQuadrant(subSector.Position))
+                    subSector.Worlds.Add(world);
+        }
+
         return false;
     }
 }
