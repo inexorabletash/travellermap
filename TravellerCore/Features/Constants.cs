6814daf9c3edd79fc2e469d1d1bbdc6a3ad1056e
Julian Silden Langlo <langlo94@gmail.com>
2021-11-02 12:34:22 +0100

    Specify axes in xml-doc.

--- TravellerCore/Features/Constants.cs
+++ TravellerCore/Features/Constants.cs
@@ -3,24 +3,24 @@ public class Constants
 {
     public class Dimensions
     {
-        /// <summary>Spinward-Trailing / Left-Right</summary>
+        /// <summary>Spinward-Trailing / Left-Right / X</summary>
         public const int SubSectorWidth = 8;
-        /// <summary>Coreward-Rimward /Up-Down</summary>
+        /// <summary>Coreward-Rimward /Up-Down / Y</summary>
         public const int SubSectorHeight = 10;
 
-        /// <summary>Spinward-Trailing / Left-Right</summary>
+        /// <summary>Spinward-Trailing / Left-Right / X</summary>
         public const int QuadrantWidth = SubSectorWidth * 2;
-        /// <summary>Coreward-Rimward /Up-Down</summary>
+        /// <summary>Coreward-Rimward /Up-Down / Y</summary>
         public const int QuadrantHeight = SubSectorHeight * 2;
 
-        /// <summary>Spinward-Trailing / Left-Right</summary>
+        /// <summary>Spinward-Trailing / Left-Right / X</summary>
         public const int SectorWidth = QuadrantWidth * 2;
-        /// <summary>Coreward-Rimward /Up-Down</summary>
+        /// <summary>Coreward-Rimward /Up-Down / Y</summary>
         public const int SectorHeight = QuadrantHeight * 2;
 
-        /// <summary>Spinward-Trailing / Left-Right</summary>
+        /// <summary>Spinward-Trailing / Left-Right / X</summary>
         public const int GalacticCenterX = 1;
-        /// <summary>Coreward-Rimward /Up-Down</summary>
+        /// <summary>Coreward-Rimward /Up-Down / Y</summary>
         public const int GalacticCenterY = 1;
 
         /// <summary>Center of the Galaxy</summary>
