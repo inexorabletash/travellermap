fb9392d7d4f5418efb26e3afba2a65f7af6282e2
Julian Silden Langlo <langlo94@gmail.com>
2021-11-02 20:03:31 +0100

    Add constants for quadrant and subsector names.

--- TravellerCore/Features/Constants.cs
+++ TravellerCore/Features/Constants.cs
@@ -26,4 +26,39 @@ public class Constants
         /// <summary>Center of the Galaxy</summary>
         public static readonly Position Center = new(GalacticCenterX, GalacticCenterY);
     }
+
+    public class Names
+    {
+        public const string QuadrantA = "Quadrant Alpha";
+        public const string QuadrantB = "Quadrant Beta";
+        public const string QuadrantC = "Quadrant Gamma";
+        public const string QuadrantD = "Quadrant Delta";
+
+        public static readonly string[] Quadrants = { QuadrantA, QuadrantB, QuadrantC, QuadrantD };
+
+        public const string SubSectorA = "Subsector A";
+        public const string SubSectorB = "Subsector B";
+        public const string SubSectorC = "Subsector C";
+        public const string SubSectorD = "Subsector D";
+        public const string SubSectorE = "Subsector E";
+        public const string SubSectorF = "Subsector F";
+        public const string SubSectorG = "Subsector G";
+        public const string SubSectorH = "Subsector H";
+        public const string SubSectorI = "Subsector I";
+        public const string SubSectorJ = "Subsector J";
+        public const string SubSectorK = "Subsector K";
+        public const string SubSectorL = "Subsector L";
+        public const string SubSectorM = "Subsector M";
+        public const string SubSectorN = "Subsector N";
+        public const string SubSectorO = "Subsector O";
+        public const string SubSectorP = "Subsector P";
+
+        public static readonly string[] SubSectors = 
+        { 
+            SubSectorA, SubSectorB, SubSectorC, SubSectorD,
+            SubSectorE, SubSectorF, SubSectorG, SubSectorH,
+            SubSectorI, SubSectorJ, SubSectorK, SubSectorL,
+            SubSectorM, SubSectorN, SubSectorO, SubSectorP,
+        };
+    }
 }
