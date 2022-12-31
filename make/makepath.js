f23f9c8b8a7efa8ab843805fa7f37567cd867ffc
Joshua Bell <inexorabletash@gmail.com>
2022-12-31 12:40:21 -0800

    Path finder: modernize JS

--- make/makepath.js
+++ make/makepath.js
@@ -1,3 +1,5 @@
+/*global Traveller, getTextViaPOST, computeRoute */
+
 "use strict";
 
 var $ = function(s) { return document.querySelector(s); };
@@ -12,62 +14,58 @@ var canvas = $('#canvas'), ctx = canvas.getContext('2d');
 
 $('#parsesec').addEventListener('click', parse);
 
-var sec;
-function parse() {
-  var data = $('#data').value;
+async function parse() {
+  const data = $('#data').value;
   if (!data.length) return;
-  getTextViaPOST(
+  const text = await getTextViaPOST(
     Traveller.MapService.makeURL('/api/sec', {type: 'TabDelimited'}),
     data
-  ).then(function(data) {
-    return parseSector(data);
-  }).then(function(sector) {
-    sec = sector;
-    var params = {
-      data: $('#data').value,
-      metadata: $('#metadata').value,
-      style: 'print',
-      options: 41975,
-      scale: 64,
-      datauri: 1
-    };
-    return getTextViaPOST(Traveller.MapService.makeURL('/api/poster'), params);
-  }).then(function(dataURL) {
-    $('#canvas').style.backgroundSize = '100% 100%';
-    $('#canvas').style.backgroundImage = 'url("' + dataURL + '")';
-    refresh();
+  );
+  const sector = sec = parseSector(text);
+  const dataURL = await getTextViaPOST(Traveller.MapService.makeURL('/api/poster'), {
+    data: $('#data').value,
+    metadata: $('#metadata').value,
+    style: 'print',
+    options: 41975,
+    scale: 64,
+    datauri: 1
   });
+  $('#canvas').style.backgroundSize = '100% 100%';
+  $('#canvas').style.backgroundImage = 'url("' + dataURL + '")';
+  refresh();
 }
 
 function parseSector(tabDelimitedData) {
-  var sector = {
+  const sector = {
     worlds: {}
   };
-  var lines = tabDelimitedData.split(/\r?\n/);
-  var header = lines.shift().toLowerCase().split('\t')
-    .map(function(h) { return h.replace(/[^a-z]/g, ''); });
-  lines.forEach(function(line) {
+  const lines = tabDelimitedData.split(/\r?\n/);
+  const header = lines.shift().toLowerCase().split('\t')
+    .map(h => h.replace(/[^a-z]/g, ''));
+  lines.forEach(line => {
     if (!line.length) return;
     var world = {};
-    line.split('\t').forEach(function(field, index) {
-      world[header[index]] = field;
-    });
+    line
+      .split('\t')
+      .forEach((field, index) => {
+        world[header[index]] = field;
+      });
     sector.worlds[world.hex] = world;
   });
   return sector;
 }
 
-var route = [];
+let route = [];
 
 function refresh() {
   function hexToCoords(hex) {
-    var x = parseFloat(hex.substring(0, 2)) - 1;
-    var y = parseFloat(hex.substring(2, 4)) - 1;
+    const x = parseFloat(hex.substring(0, 2)) - 1;
+    const y = parseFloat(hex.substring(2, 4)) - 1;
     return hxhyToCoords(x, y);
   }
   function hxhyToCoords(hx, hy) {
-    var x = hx, y = hy;
-    var dy = (x % 2) ? 0.5 : 0;
+    let x = hx, y = hy;
+    const dy = (x % 2) ? 0.5 : 0;
     x *= Math.cos(Math.PI/6); // cos(30deg)
     return {x:x*PS+INSET+PS/2, y:(y+dy)*PS+INSET+PS/2};
   }
@@ -76,8 +74,8 @@ function refresh() {
 
   ctx.lineWidth = 2;
   ctx.strokeStyle = 'red';
-  stack.forEach(function(hex) {
-    var coords = hexToCoords(hex);
+  stack.forEach(hex => {
+    const coords = hexToCoords(hex);
     ctx.beginPath();
     ctx.arc(coords.x,
             coords.y,
@@ -85,12 +83,12 @@ function refresh() {
     ctx.stroke();
   });
 
-  var out = '';
+  let out = '';
   if (route) {
     ctx.lineWidth = 2;
     ctx.strokeStyle = 'rgba(0,128,0,0.5)';
-    route.forEach(function(hex, index) {
-      var coords = hexToCoords(hex);
+    route.forEach((hex, index) => {
+      const coords = hexToCoords(hex);
       ctx.beginPath();
       ctx.arc(coords.x,
               coords.y,
@@ -101,8 +99,8 @@ function refresh() {
     ctx.lineWidth = 4;
     ctx.strokeStyle = "green";
     ctx.beginPath();
-    route.forEach(function(hex, index) {
-      var coords = hexToCoords(hex);
+    route.forEach((hex, index) => {
+      const coords = hexToCoords(hex);
       if (index === 0) {
         ctx.moveTo(coords.x, coords.y);
         out += hex;
@@ -119,29 +117,29 @@ function refresh() {
 }
 
 
-var stack = [];
-$('#canvas').addEventListener('mousedown', function(e) {
+const stack = [];
+$('#canvas').addEventListener('mousedown', e => {
   e.preventDefault();
   e.stopPropagation();
 
-  var offsetX = 'offsetX' in e ? e.offsetX :
+  const offsetX = 'offsetX' in e ? e.offsetX :
     'layerX' in e ? e.layerX :
     e.pageX - e.target.offsetLeft;
-  var offsetY = 'offsetY' in e ? e.offsetY :
+  const offsetY = 'offsetY' in e ? e.offsetY :
     'layerY' in e ? e.layerY :
     e.pageY - e.target.offsetTop;
-  var x = offsetX, y = offsetY;
+  let x = offsetX, y = offsetY;
 
   x = (x - INSET) / PS / Math.cos(Math.PI/6);
   y = (y - INSET) / PS;
   x = Math.floor(x);
   if (x % 2) y -= 0.5;
   y = Math.floor(y);
-  var hex = ('00' + (x+1)).slice(-2) + ('00' + (y+1)).slice(-2);
+  const hex = ('00' + (x+1)).slice(-2) + ('00' + (y+1)).slice(-2);
 
   if (stack.length) {
-    var start = stack.pop();
-    var jump = Number($('#jump').value);
+    const start = stack.pop();
+    const jump = Number($('#jump').value);
     route = computeRoute(sec.worlds, start, hex, jump);
   } else {
     stack.push(hex);
