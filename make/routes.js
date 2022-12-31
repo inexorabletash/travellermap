1d8600fc1bc60547ac30f99cd2574e4c9721c66f
Joshua Bell <inexorabletash@gmail.com>
2022-12-31 14:47:44 -0800

    Route maker: Add autogenerate/cleanup options

--- make/routes.js
+++ make/routes.js
@@ -8,8 +8,8 @@ const PS = 16; // px/parsec
 const INSET = 2; // px
 const RADIUS = 4;
 
-let sec = {};
-const routes = [];
+let sec = {worlds:{}};
+let routes = [];
 
 const canvas = $('#canvas'), ctx = canvas.getContext('2d');
 
@@ -39,6 +39,7 @@ async function parse() {
       });
     $('#canvas').style.backgroundSize = '100% 100%';
     $('#canvas').style.backgroundImage = 'url("' + dataURL + '")';
+    routes = [];
     refresh();
   } catch (reason) {
     alert('Server error: ' + reason);
@@ -68,20 +69,19 @@ function parseSector(tabDelimitedData) {
   return sector;
 }
 
+function hexToCoords(hex) {
+  const x = parseFloat(hex.substring(0, 2)) - 1;
+  const y = parseFloat(hex.substring(2, 4)) - 1;
+  return hxhyToCoords(x, y);
+}
+function hxhyToCoords(hx, hy) {
+  let x = hx, y = hy;
+  const dy = (x % 2) ? 0.5 : 0;
+  x *= Math.cos(Math.PI/6); // cos(30deg)
+  return {x:x*PS+INSET+PS/2, y:(y+dy)*PS+INSET+PS/2};
+}
 
 function refresh() {
-  function hexToCoords(hex) {
-    const x = parseFloat(hex.substring(0, 2)) - 1;
-    const y = parseFloat(hex.substring(2, 4)) - 1;
-    return hxhyToCoords(x, y);
-  }
-  function hxhyToCoords(hx, hy) {
-    let x = hx, y = hy;
-    const dy = (x % 2) ? 0.5 : 0;
-    x *= Math.cos(Math.PI/6); // cos(30deg)
-    return {x:x*PS+INSET+PS/2, y:(y+dy)*PS+INSET+PS/2};
-  }
-
   ctx.clearRect(0, 0, PS * canvas.width, PS * canvas.height);
 
   ctx.lineWidth = 2;
@@ -166,6 +166,132 @@ function clear() {
   stack.length = 0;
   routes.length = 0;
   refresh();
+}
+
+$('#auto-kk').addEventListener('click', () => { auto('kk'); });
+$('#auto-zh').addEventListener('click', () => { auto('zh'); });
+
+function auto(t) {
+  switch (t) {
+  case 'kk':
+    autoConnect(Object.values(sec.worlds).filter(world => {
+      if (!world.allegiance.startsWith('Kk')) return false;
+      if (world.uwp[0] !== 'A') return false;
+      return true;
+    }), 3);
+    break;
+  case 'zh':
+    autoConnect(Object.values(sec.worlds).filter(world => {
+      if (!world.allegiance.startsWith('Zh')) return false;
+      return ['K', 'M', 'D', 'W'].some(b => world.bases.includes(b));
+    }), 4);
+    break;
+  }
+}
+
+function autoConnect(worlds, range) {
+  // Examine each pair
+  for (let i = 0; i < worlds.length - 1; ++i) {
+    for (let j = i + 1; j < worlds.length; ++j) {
+      const hex1 = worlds[i].hex,
+            hex2 = worlds[j].hex;
+      if (dist(hex1, hex2) <= range) {
+        routes.push({start: hex1, end: hex2});
+      }
+    }
+  }
+  removeIntersections();
+  refresh();
+}
+
+// Distance in hexes
+// dist('0101', '0404') -> 5
+function dist(a, b) {
+  a = Number(a);
+  b = Number(b);
+  const a_x = div(a,100);
+  const a_y = mod(a,100);
+  const b_x = div(b,100);
+  const b_y = mod(b,100);
+
+  const dx = b_x - a_x;
+  const dy = b_y - a_y;
+
+  let adx = Math.abs(dx);
+  let ody = dy + div(adx, 2);
+
+  if (odd(a_x) && even(b_x))
+    ody += 1;
+
+  return max(adx - ody, ody, adx);
+
+  function even(x) { return (x % 2) == 0; }
+  function odd (x) { return (x % 2) != 0; }
+
+  function div(a, b) { return Math.floor(a / b); }
+  function mod(a, b) { return Math.floor(a % b); }
+
+  function max(a, b, c) { return (a >= b && a >= c) ? a : (b >= a && b >= c) ? b : c; }
+}
+
+
+$('#nointersect').addEventListener('click', () => { removeIntersections(); refresh(); });
+function removeIntersections() {
+  const len = r => dist(r.start, r.end);
+
+  // Remove degenerate routes
+  routes = routes.filter(route => len(route) > 0);
+
+  // Sort by length (reversed), so shortest routes are removed
+  routes.sort((a,b) => len(b) - len(a));
+
+  // Remove any intersecting routes
+  for (let i = 0; i < routes.length - 1; ++i) {
+    for (let j = i + 1; j < routes.length;) {
+      if (intersects(routes[i], routes[j])) {
+        routes.splice(j, 1);
+      } else {
+        ++j;
+      }
+    }
+  }
+
+  function intersects(route1, route2) {
+    // Filter identical routes
+    if (route1.start === route2.start && route1.end === route2.end) return true;
+    if (route1.start === route2.end && route1.end === route2.start) return true;
+
+    // But allow endpoints to touch
+    if (route1.start === route2.start) return false;
+    if (route1.end   === route2.end  ) return false;
+    if (route1.start === route2.end  ) return false;
+    if (route1.end   === route2.start) return false;
+
+    // Look for true intersections
+    const coords0 = hexToCoords(route1.start);
+    const coords1 = hexToCoords(route1.end);
+    const coords2 = hexToCoords(route2.start);
+    const coords3 = hexToCoords(route2.end);
+
+    return segmentIntersect(coords0.x, coords0.y,
+                            coords1.x, coords1.y,
+                            coords2.x, coords2.y,
+                            coords3.x, coords3.y);
+  }
+
+
+  // https://stackoverflow.com/questions/563198/how-do-you-detect-where-two-line-segments-intersect
+  function segmentIntersect(p0_x, p0_y, p1_x, p1_y, p2_x, p2_y, p3_x, p3_y) {
+    let s1_x, s1_y, s2_x, s2_y;
+    s1_x = p1_x - p0_x;     s1_y = p1_y - p0_y;
+    s2_x = p3_x - p2_x;     s2_y = p3_y - p2_y;
+
+    let s, t;
+    s = (-s1_y * (p0_x - p2_x) + s1_x * (p0_y - p2_y)) / (-s2_x * s1_y + s1_x * s2_y);
+    t = ( s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (-s2_x * s1_y + s1_x * s2_y);
+
+    return (s >= 0 && s <= 1 && t >= 0 && t <= 1);
+  }
 }
 
 parse();
