b064ad0de52ea69cf4c10446cf0019c1dce6760b
Joshua Bell <inexorabletash@gmail.com>
2023-07-08 08:30:05 -0700

    Page: Add Travellercon/USA 2023 promo

--- index.js
+++ index.js
@@ -1665,7 +1665,7 @@
   // Show promo, if not dismissed.
   if (!isIframe) {
     setTimeout(() => {
-      const promo_key = 'tm_promo4';
+      const promo_key = 'tm_promo5';
       if (!localStorage.getItem(promo_key)) {
         document.body.classList.add('show-promo');
         $('#promo-closebtn').addEventListener('click', event => {
@@ -1674,7 +1674,7 @@
         });
         $$('#promo-hover a').forEach(a => a.addEventListener('click', event => {
           document.body.classList.remove('show-promo');
-          //localStorage.setItem(promo_key, 1);
+          localStorage.setItem(promo_key, 1);
         }));
       }
     }, 1000);
