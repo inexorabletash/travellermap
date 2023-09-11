inexorabletash/travellermap

commit 46b198a75265f5bc17eaeb1a45e9f0efeee9e96e

Server: Hack to prevent image gaps in Faraway tiles

server/RenderContext.cs
@@ -336,14 +336,19 @@ namespace Maps.Rendering
                 AbstractGraphicsState? state = null;
                 foreach (var layer in layers)
                 {
+                    // HACK: Clipping to tileRect rapidly becomes inaccurate away from
+                    // the origin due to float precision. Only do it if really necessary.
+                    bool clip = layer.clip &&
+                        !((ClipPath == null) && (graphics is BitmapGraphics));
+
                     // Impose a clipping region if desired, or remove it if not.
-                    if (layer.clip && state == null)
+                    if (clip && state == null)
                     {
                         state = graphics.Save();
                         if (ClipPath != null) graphics.IntersectClip(ClipPath);
                         else graphics.IntersectClip(tileRect);
                     }
-                    else if (!layer.clip && state != null)
+                    else if (!clip && state != null)
                     {
                         state.Dispose();
                         state = null;
@@ -719,7 +724,12 @@ namespace Maps.Rendering
             {
                 graphics.SmoothingMode = SmoothingMode.HighSpeed;
                 solidBrush.Color = styles.backgroundColor;
-                graphics.DrawRectangle(solidBrush, tileRect);
+
+                // HACK: Due to limited precisions of floats, tileRect can end up not covering
+                // the full bitmap when far from the origin.
+                var rect = tileRect;
+                rect.Inflate(rect.Width * 0.1f, rect.Height * 0.1f);
+                graphics.DrawRectangle(solidBrush, rect);
             }
 
             private void OverlayGlyph(string glyph, AbstractFont font, Point coordinates)

commit 95f3baaf5aae81e33e4a2198501b663929214320

Rendering: Fix clipping with clampar option
Broken in 46b198a75 which scoped clipping too much. Force the clipping
to happen if the clampar option is passed.

server/RenderContext.cs
@@ -49,6 +49,7 @@ namespace Maps.Rendering
         public AbstractPath? ClipPath { get; set; }
         public bool DrawBorder { get; set; }
         public bool ClipOutsectorBorders { get; set; }
+        public bool ForceClip { get; set; }
 
         public Stylesheet Styles => styles;
         private readonly AbstractMatrix worldSpaceToImageSpace;
@@ -338,8 +339,8 @@ namespace Maps.Rendering
                 {
                     // HACK: Clipping to tileRect rapidly becomes inaccurate away from
                     // the origin due to float precision. Only do it if really necessary.
-                    bool clip = layer.clip &&
-                        !((ClipPath == null) && (graphics is BitmapGraphics));
+                    bool clip = layer.clip && (ctx.ForceClip ||
+                        !((ClipPath == null) && (graphics is BitmapGraphics)));
 
                     // Impose a clipping region if desired, or remove it if not.
                     if (clip && state == null)

server/api/PosterHandler.cs
@@ -271,6 +271,8 @@ namespace Maps.API
                 if (hrot != 0)
                     ApplyHexRotation(hrot, stylesheet, ref bitmapSize, ref hexTransform);
 
+                bool transparent = false;
+                bool forceClip = false;
                 AbstractMatrix clampTransform = AbstractMatrix.Identity;
                 if (GetBoolOption("clampar", defaultValue: false))
                 {
@@ -294,6 +296,8 @@ namespace Maps.API
                             (newSize.Width - bitmapSize.Width) / 2f,
                             (newSize.Height - bitmapSize.Height) / 2f);
                         bitmapSize = newSize;
+                        transparent = true;
+                        forceClip = true;
                     }
                 }
 
@@ -306,9 +310,10 @@ namespace Maps.API
 
                 RenderContext ctx = new RenderContext(resourceManager, selector, tileRect, scale, options, stylesheet, tileSize)
                 {
+                    ForceClip = forceClip,
                     ClipOutsectorBorders = clipOutsectorBorders
                 };
-                ProduceResponse(Context, title, ctx, bitmapSize, transform);
+                ProduceResponse(Context, title, ctx, bitmapSize, transform, transparent);
             }
         }
     }